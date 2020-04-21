53bdd86b6ad1ed5f2ff0cc22d093a5a97cecaa3f
Cass <qwerto227@gmail.com>
2020-04-21 23:48:44 +0100

    Created Basic Skeleton (Topdown render, WASD Movement)

---
+++ Magitech/ClassBin/Keybinds.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Magitech
+{
+
+    class Keybinds
+    {
+        public Keys PlayerMoveUp = Keys.W;
+        public Keys PlayerMoveDown = Keys.S;
+        public Keys PlayerMoveLeft = Keys.A;
+        public Keys PlayerMoveRight = Keys.D;
+        public Keys CameraMoveUp = Keys.Up;
+        public Keys CameraMoveDown = Keys.Down;
+        public Keys CameraMoveLeft = Keys.Left;
+        public Keys CameraMoveRight = Keys.Right;
+        public Keys ExitGame = Keys.Escape;
+    };
+}
---
+++ Magitech/ClassBin/entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Magitech
+{
+    public class Entity
+    {
+        //The position and the speed of the entity. null if Entity does not have a position
+        public Vector2 position;
+        public float speed;
+
+        public Texture2D texture;
+        public string texturename; //The name of the texture
+
+
+        public Entity()
+        {
+        }
+    }
+}
